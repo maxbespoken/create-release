302f9fcb608363a5dce595895b3a78dd45ce181c
Maximo Rosales Diaz <max@bespoken.io>
2023-03-02 17:44:41 -0500

    WIP

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+const { Octokit } = require('octokit')
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function main() {
+    const octokit = new Octokit({
+        auth: core.getInput('token'),
+    });
+
+    const owner = "bespoken"
+    const repo = "iac"
+
+    const list = await octokit.rest.repos.listReleases({
+        owner, repo
+    })
+    
+    console.log('All', list)
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 }
+
+main().catch(err => core.setFailed(error.message))
